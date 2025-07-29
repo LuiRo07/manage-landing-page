1a0bbb290d5abbde1a95c0419c7c118dde74ce1c
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2025-07-29 13:43:50 -0700

    bulelt point section in the CTA is responsive, needs bg-color to match design

--- src/components/cta-section/CTASection.jsx
+++ src/components/cta-section/CTASection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './styles.css'
 
 // this section highlights the key features of the Manage software
 function CTASection() {
@@ -13,26 +14,32 @@ function CTASection() {
         </article>
         {/* right side */}
         <article className="sm:w-[40%]">
-            <div className="grid grid-cols-[auto_90%] gap-x-4 mb-4">
-                <span className="bg-primary-red text-white px-6 py-2 rounded-full max-w-fit">01</span>
-                <h1 className="text-lg text-primary-blue mb-2 bg-primary-red/15 sm:bg-transparent"> Track company-wide progress</h1>
-                <p className="col-start-2 max-w-[70%]">See how your day-to-day tasks fit into the wider vision.
+            <div className="flex flex-col mb-8">
+                <div className="bg-[cornsilk] sm:bg-transparent flex items-center rounded-l-full mb-2 sm:mb-4">
+                    <span className="bg-primary-red text-white w-[75px] h-[50px] inline-flex items-center justify-center rounded-full mr-4">01</span>
+                    <h1 className=" text-primary-blue font-bold mb-2 sm:bg-transparent"> Track company-wide progress</h1>
+                </div>
+                <p className="sm:ml-24">See how your day-to-day tasks fit into the wider vision.
                     Go from tracking progress at the milestone level all the way done 
                     to the smallest of details. Never lose sight of the 
                     bigger picture again.
                 </p>
             </div>
-            <div className="grid grid-cols-[auto_90%] gap-x-4 mb-4">
-                <span className="bg-primary-red text-white px-6 py-2 rounded-full max-w-fit">02</span>
-                <h1 className="text-lg text-primary-blue mb-2"> Advanced built-in reports</h1>
-                <p className="col-start-2 max-w-[70%]">Set internal delivery estimates and track progress toward company goals. Our customisable
+            <div className="flex flex-col mb-8">
+                <div className="bg-[cornsilk] sm:bg-transparent flex sm: items-center rounded-l-full mb-2 sm:mb-4">
+                    <span className="bg-primary-red text-white w-[75px] h-[50px] inline-flex items-center justify-center rounded-full mr-4">02</span>
+                    <h1 className="text-primary-blue mb-2 font-bold sm:bg-transparent"> Advanced built-in reports</h1>
+                </div>
+                <p className="sm:ml-24">Set internal delivery estimates and track progress toward company goals. Our customisable
                     dashboard helps you build out the reports you need to keep key stakeholders informed.
                 </p>
             </div>
-            <div className="grid grid-cols-[auto_90%] gap-x-4 mb-4">
-                <span className="bg-primary-red text-white px-6 py-2 rounded-full max-w-fit">03</span>
-                <h1 className="text-lg text-primary-blue mb-2">Everything you need in one place</h1>
-                <p className="col-start-2 max-w-[70%]">Stop jumping from one service to another to communicate, store files, and share documents.
+            <div className="flex flex-col mb-8">
+                <div className="bg-[cornsilk] sm:bg-transparent flex sm: items-center rounded-l-full mb-2 sm:mb-4 overflow-hidden">
+                    <span className="bg-primary-red text-white w-[75px] h-[50px] inline-flex items-center justify-center rounded-full mr-4">03</span>
+                    <h1 className="text-primary-blue font-bold text-nowrap mb-2 sm:bg-transparent">Everything you need in one place</h1>
+                </div>
+                <p className="sm:ml-24">Stop jumping from one service to another to communicate, store files, and share documents.
                     Manage offers an all-in-one team productivity solution.
                 </p>
             </div>
--- src/components/intro-section/IntroSection.jsx
+++ src/components/intro-section/IntroSection.jsx
@@ -5,7 +5,7 @@ import CTASection from '../cta-section/CTASection'
 // contains the navbar, hero section, and bullet points
 function IntroSection() {
   return (
-    <section className="bg-[url('/images/overlay-pattern/bg-simplify-section-desktop.svg')] bg-cover bg-no-repeat px-12">
+    <section className="bg-[url('/images/overlay-pattern/bg-simplify-section-desktop.svg')] bg-cover bg-no-repeat px-10">
       <NavBar />
       <HeroSection />
       <CTASection />
