7ddb14c364dafb430f0b3824707129c2edb70498
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2025-11-04 18:24:28 -0800

    body text is responsive

--- src/components/testimonial-section/Card.jsx
+++ src/components/testimonial-section/Card.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 
 function Card({image, name, paragraph}) {
   return (
-    <>  
-        <div className="flex justify-center relative top-7">
-            <img className="w-18 h-18 rounded-full" src={image} alt={"Avatar images"} />
+    <div className="h-[380px] flex flex-col">  
+        {/* profile image */}
+        <div className="flex justify-center pt-6 relative top-7">
+            <img className="w-16 h-16 sm:w-18 sm:h-18 rounded-full" src={image} alt={"Avatar images"} />
         </div>
-        <div className="bg-[#9f9fa940] p-6 text-center">
-            <h2 className="text-primary-blue font-bold text-xl my-6">{name}</h2>
-            <p className="text-gray-500">{paragraph}</p>
+        {/* text body */}
+        <div className="bg-[#9f9fa940] p-4 sm:p-6 text-center flex-1 flex flex-col justify-center overflow-hidden">
+          <h2 className="text-primary-blue font-bold text-base sm:text-xl lg:text-xl mb-3 sm:mb-4">{name}</h2>
+          <p className="text-gray-500 sm:text-sm lg:text-base leading-relaxed line-clamp-4">{paragraph}</p>
         </div>
-    </>
+    </div>
   )
 }
 
